3d50dc396b90fcc380595126a7f375167dfca4c8
adambudner <adambudner@outlook.com>
2026-04-08 10:04:29 +0200

    completed ui

--- appClient.py
+++ appClient.py
@@ -46,9 +46,42 @@ class MainWindowUi(QtWidgets.QMainWindow):
         self.verticalLayoutRight.setSpacing(15)
         self.verticalLayoutRight.setObjectName("verticalLayoutRight")
         
+        # wypozyczenia
+        self.group_wypozyczenia = QtWidgets.QGroupBox(parent=self.centralwidget)
+        self.group_wypozyczenia.setObjectName("group_wypozyczenia")
+        self.layout_wyp = QtWidgets.QVBoxLayout(self.group_wypozyczenia)
+        self.layout_wyp.setObjectName("layout_wyp")
         
+        self.labelDodajWyp = QtWidgets.QLabel(parent=self.group_wypozyczenia)
+        self.labelDodajWyp.setObjectName("labelDodajWyp")
+        self.layout_wyp.addWidget(self.labelDodajWyp)
         
+        self.combo_wyszukaj_gre = QtWidgets.QComboBox(parent=self.group_wypozyczenia)
+        self.combo_wyszukaj_gre.setEditable(True)
+        self.combo_wyszukaj_gre.setInsertPolicy(QtWidgets.QComboBox.InsertPolicy.NoInsert)
+        self.combo_wyszukaj_gre.setObjectName("combo_wyszukaj_gre")
+        self.layout_wyp.addWidget(self.combo_wyszukaj_gre)
         
+        self.btn_dodaj_wyp = QtWidgets.QPushButton(parent=self.group_wypozyczenia)
+        self.btn_dodaj_wyp.setObjectName("btn_dodaj_wyp")
+        self.layout_wyp.addWidget(self.btn_dodaj_wyp)
+        
+        spacerItem = QtWidgets.QSpacerItem(20, 10, QtWidgets.QSizePolicy.Policy.Minimum, QtWidgets.QSizePolicy.Policy.Fixed)
+        self.layout_wyp.addItem(spacerItem)
+        
+        self.labelUsunWyp = QtWidgets.QLabel(parent=self.group_wypozyczenia)
+        self.labelUsunWyp.setObjectName("labelUsunWyp")
+        self.layout_wyp.addWidget(self.labelUsunWyp)
+        
+        self.inp_usun_wyp_id = QtWidgets.QLineEdit(parent=self.group_wypozyczenia)
+        self.inp_usun_wyp_id.setObjectName("inp_usun_wyp_id")
+        self.layout_wyp.addWidget(self.inp_usun_wyp_id)
+        
+        self.btn_usun_wyp = QtWidgets.QPushButton(parent=self.group_wypozyczenia)
+        self.btn_usun_wyp.setObjectName("btn_usun_wyp")
+        self.layout_wyp.addWidget(self.btn_usun_wyp)
+        
+        self.verticalLayoutRight.addWidget(self.group_wypozyczenia)
         
         # end
         self.verticalLayoutRight.addWidget(self.group_gry)
